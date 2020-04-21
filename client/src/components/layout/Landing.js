8d029ec6dfbddd63b55f69d1a7f687227bf455d7
Jenya <jekab596@gmail.com>
2020-04-21 17:30:50 +0300

    initialization of the client side, installation of packages needed for work

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section>
+      <div>
+        <h1>healthy people connector</h1>
+        <p>talk to other people who interesting to improve their lives</p>
+        <div>
+          <div>Sign Up</div>
+          <div>Login</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Evolution</h1>
+      <ul>
+        <li>somthing</li>
+        <li>Register</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
