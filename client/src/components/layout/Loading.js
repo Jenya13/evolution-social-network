85a264b7c53ec8ac9b5db450ac9701e1f64e9873
Jenya <jekab596@gmail.com>
2020-09-22 05:13:44 +0300

    Styling spinner

--- client/src/components/layout/Loading.js
+++ client/src/components/layout/Loading.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Loading = () => {
   return (
     <Fragment>
-      <h3>Loading...</h3>
+      <div className='loader'></div>
     </Fragment>
   );
 };
